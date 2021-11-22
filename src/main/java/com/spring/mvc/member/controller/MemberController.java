2a4aea83d60f204dcfe324551d53f1c9c423b046
kokokaki <hsg9984@gmail.com>
2021-11-22 15:44:39 +0900

    [21.11.19] # Member APP - 회원가입, 중복확인

--- src/main/java/com/spring/mvc/member/controller/MemberController.java
+++ src/main/java/com/spring/mvc/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.spring.mvc.member.controller;
 
+import com.spring.mvc.member.domain.Member;
 import com.spring.mvc.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +20,30 @@ public class MemberController {
     @GetMapping("/join")
     public String join() {
         return "member/join";
+    }
+
+    //아이디 중복확인 비동기 통신요청
+    @GetMapping("/check")
+    @ResponseBody
+    public boolean check(String checkId) {
+        log.info("/check 비동기 요청 GET! " + checkId);
+        return memberService.isDuplicate(checkId);
+    }
+
+    //이메일 중복확인 비동기 통신요청
+    @GetMapping("/check2")
+    @ResponseBody
+    public boolean check2(String checkEmail) {
+        log.info("/check2 비동기 요청 GET! " + checkEmail);
+        return memberService.isDuplicate2(checkEmail);
+    }
+
+    //회원가입 요청 처리
+    @PostMapping("/member/sign-up")
+    public String signUp(Member member) {
+        log.info("/member/sign-up POST! - " + member);
+        //서비스 위임
+        memberService.signUp(member);
+        return "redirect:/";
     }
 }
--- src/main/java/com/spring/mvc/member/service/MemberService.java
+++ src/main/java/com/spring/mvc/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.spring.mvc.member.service;
 
+import com.spring.mvc.member.domain.Member;
 import com.spring.mvc.member.repository.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,22 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberMapper memberMapper;
+
+    //아이디 중복확인 중간처리
+    public boolean isDuplicate(String checkId) {
+        return memberMapper.isDuplicate(checkId) == 1;
+    }
+    //이메일 중복확인 중간처리
+    public boolean isDuplicate2(String checkEmail) {
+        return memberMapper.isDuplicate2(checkEmail) == 1;
+    }
+
+    public void signUp(Member member) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPw = encoder.encode(member.getPassword());
+        member.setPassword(encodedPw);
+
+        memberMapper.register(member);
+    }
 }
